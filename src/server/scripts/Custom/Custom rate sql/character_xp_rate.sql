eef71a1cc77d32229beccbebae3c80d73b84505d
quadral <airhawk@hotmail.co.uk>
2015-09-07 20:44:44 +0100

    Added world database and fixed sqls in custom scripts

--- src/server/scripts/Custom/Custom rate sql/character_xp_rate.sql
+++ src/server/scripts/Custom/Custom rate sql/character_xp_rate.sql
@@ -3,4 +3,4 @@ CREATE TABLE `character_xp_rate` (
   `guid` int(10) UNSIGNED NOT NULL,
   `xp_rate` int(10) UNSIGNED NOT NULL DEFAULT '1',
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Player XP Rate System';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Player XP Rate System';ant_world
