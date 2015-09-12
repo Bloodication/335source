521bb73ddff11c64c297aca843149d00b00f42ce
quadral <airhawk@hotmail.co.uk>
2015-09-12 15:19:34 +0100

    [DB/Auth] Added additional sql for new rbac for custom rate

--- src/server/scripts/Custom/CustomRate/sql/authcommands.sql
+++ src/server/scripts/Custom/CustomRate/sql/authcommands.sql
@@ -1,5 +1,14 @@
 INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1008, 'Command: xp');
-
+(1008, 'Command: Rate xp');
 INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
 (195, 1008);
+
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
+(1010, 'Command: Rate loot');
+INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
+(195, 1010);
+
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
+(1011, 'Command: Rate');
+INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
+(195, 1011);
