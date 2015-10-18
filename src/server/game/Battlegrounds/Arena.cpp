c6fe5bfcd737022c784888beea9f9c0ba79e3c5c
quadral <airhawk@hotmail.co.uk>
2015-10-18 22:30:48 +0100

    [Core/Arena] Arena Solo3v3 Fix

--- src/server/game/Battlegrounds/Arena.cpp
+++ src/server/game/Battlegrounds/Arena.cpp
@@ -94,7 +94,7 @@ void Arena::HandleKillPlayer(Player* player, Player* killer)
 
 void Arena::RemovePlayerAtLeave(ObjectGuid guid, bool transport, bool sendPacket)
 {
-    if (isRated() && GetStatus() == STATUS_IN_PROGRESS)
+	if (isRated() || GetArenaType() == ARENA_TYPE_3v3_SOLO && GetStatus() == STATUS_IN_PROGRESS)
     {
         BattlegroundPlayerMap::const_iterator itr = m_Players.find(guid);
         if (itr != m_Players.end()) // check if the player was a participant of the match, or only entered through gm command (appear)
