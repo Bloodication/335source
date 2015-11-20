6eb71f7336618c47956919723556e6e09e116d71
quadral <airhawk@hotmail.co.uk>
2015-11-20 11:50:59 +0000

    fix

---
+++ sql/updates/characters/2015_10_13_characters.sql
@@ -0,0 +1,68 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.26-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.2.0.4981
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table 19_characters.auctionhouse
+DROP TABLE IF EXISTS `auctionhouse`;
+CREATE TABLE IF NOT EXISTS `auctionhouse` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0',
+  `auctioneerguid` int(10) unsigned NOT NULL DEFAULT '0',
+  `itemguid` int(10) unsigned NOT NULL DEFAULT '0',
+  `itemowner` int(10) unsigned NOT NULL DEFAULT '0',
+  `buyoutprice` int(10) unsigned NOT NULL DEFAULT '0',
+  `time` int(10) unsigned NOT NULL DEFAULT '0',
+  `buyguid` int(10) unsigned NOT NULL DEFAULT '0',
+  `lastbid` int(10) unsigned NOT NULL DEFAULT '0',
+  `startbid` int(10) unsigned NOT NULL DEFAULT '0',
+  `deposit` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `item_guid` (`itemguid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table 19_characters.auctionhouse: ~0 rows (approximately)
+/*!40000 ALTER TABLE `auctionhouse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auctionhouse` ENABLE KEYS */;
+
+
+-- Dumping structure for table 19_characters.corpse
+DROP TABLE IF EXISTS `corpse`;
+CREATE TABLE IF NOT EXISTS `corpse` (
+  `corpseGuid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+  `guid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Character Global Unique Identifier',
+  `posX` float NOT NULL DEFAULT '0',
+  `posY` float NOT NULL DEFAULT '0',
+  `posZ` float NOT NULL DEFAULT '0',
+  `orientation` float NOT NULL DEFAULT '0',
+  `mapId` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'Map Identifier',
+  `phaseMask` int(10) unsigned NOT NULL DEFAULT '1',
+  `displayId` int(10) unsigned NOT NULL DEFAULT '0',
+  `itemCache` text NOT NULL,
+  `bytes1` int(10) unsigned NOT NULL DEFAULT '0',
+  `bytes2` int(10) unsigned NOT NULL DEFAULT '0',
+  `guildId` int(10) unsigned NOT NULL DEFAULT '0',
+  `flags` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `dynFlags` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `time` int(10) unsigned NOT NULL DEFAULT '0',
+  `corpseType` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `instanceId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Instance Identifier',
+  PRIMARY KEY (`corpseGuid`),
+  KEY `idx_type` (`corpseType`),
+  KEY `idx_instance` (`instanceId`),
+  KEY `idx_player` (`guid`),
+  KEY `idx_time` (`time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Death System';
+
+-- Dumping data for table 19_characters.corpse: ~0 rows (approximately)
+/*!40000 ALTER TABLE `corpse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `corpse` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
